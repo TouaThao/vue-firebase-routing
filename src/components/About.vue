aeea8dca52ff7c454824278ae584bcc88e269acc
Toua Thao <touathao09@gmail.com>
2019-05-31 14:30:47 -0500

    added route and about

---
+++ src/components/About.vue
@@ -0,0 +1,23 @@
+<template>
+   <div class="about">
+       <h1>
+           about
+       </h1>
+   </div>
+</template> 
+
+<script>
+export default {
+    name:'about',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
