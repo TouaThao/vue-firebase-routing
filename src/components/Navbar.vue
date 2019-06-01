0cf7fdcfffa5151ec196b6f55c1293188ad81791
Toua Thao <touathao09@gmail.com>
2019-06-01 13:46:38 -0500

    finish goHome button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,10 +2,26 @@
   <div class="Nav">
     <ul>
       <li>
-          <router-link to="/">Home</router-link>
+        <router-link to="/">Home</router-link>
       </li>
       <li>
-          <router-link :to="{ name: 'About'}">About</router-link>
+        <router-link :to="{ name: 'About'}">About</router-link>
+      </li>
+      <h2>user-Profile</h2>
+      <ul>
+        <li v-for="(id,index) in userIds" :key="index">
+          <router-link :to="{name:'ViewProfile', params:{user_id:id}}">
+            <span>Profile {{ id }}</span>
+          </router-link>
+        </li>
+      </ul>
+    </ul>
+    <h2>
+      Navigation control
+    </h2>
+    <ul>
+      <li>
+        <button @click="goHome"> Redirect to Home</button>
       </li>
     </ul>
   </div>
@@ -15,18 +31,30 @@
 export default {
   name: "Nav",
   data() {
-    return {};
+    return {
+      userIds: ["1", "2", "3", "4"]
+    };
+  },
+  methods:{
+    goHome(){
+      /* route vs router. this.$route is the current route your in
+      this.$router keep track of all our navagation history
+      */
+      this.$router.push({
+        name:'Home'
+      })
+    }
   }
 };
 </script>
 
 
 <style>
-ul{
-    list-style-type: none;
-    padding:0
+ul {
+  list-style-type: none;
+  padding: 0;
 }
-a{
-    color:#42b983
+a {
+  color: #42b983;
 }
 </style>
