0eae21a8c1820a0a2c7f4983480f58b9ea202a62
Toua Thao <touathao09@gmail.com>
2019-06-01 09:46:24 -0500

    dynamic route

---
+++ src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="Nav">
+    <ul>
+      <li>
+          <router-link to="/">Home</router-link>
+      </li>
+      <li>
+          <router-link :to="{ name: 'About'}">About</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+  data() {
+    return {};
+  }
+};
+</script>
+
+
+<style>
+ul{
+    list-style-type: none;
+    padding:0
+}
+a{
+    color:#42b983
+}
+</style>
